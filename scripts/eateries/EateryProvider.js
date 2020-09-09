9485b20042b5ec86173a89fe51f5205e9b805a1b
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-09-09 13:19:59 -0500

    Initial setups

---
+++ scripts/eateries/EateryProvider.js
@@ -0,0 +1,18 @@
+const eventHub = document.querySelector(".container")
+
+let eateries = []
+
+export const useEateries = () => {
+    return eateries.slice()
+}
+
+export const getEateries = () => {
+    return fetch("http://holidayroad.nss.team/eateries")
+        .then(response => response.json())
+        .then(
+            parsedEateries => {
+                console.table(parsedEateries)
+                eateries = parsedEateries
+            }
+        )
+}
