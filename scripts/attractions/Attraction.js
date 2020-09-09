59e66187c1ce2e6f0fee8bee34198d504a6b3559
Ricky Blake <rab0196@yahoo.com>
2020-09-09 15:10:06 -0500

    added id to button

--- scripts/attractions/Attraction.js
+++ scripts/attractions/Attraction.js
@@ -2,7 +2,7 @@ export const AttractionHTML = attractionObj => {
     return `
         <div class="attraction">
             <h3>${attractionObj.attractionThatWasChosen}</h3>
-            <button type="button" class="detailsBtn">Show Details</button>
+            <button type="button" class="detailsBtn" id="detailsBtn-attraction">Show Details</button>
         </div>
     `
 }
