291050c7466b8994eae8936566a3a230a99e3f26
Ricky Blake <rab0196@yahoo.com>
2020-09-11 14:10:37 -0500

    bolded descriptive text

--- scripts/itineraries/Itinerary.js
+++ scripts/itineraries/Itinerary.js
@@ -3,9 +3,9 @@ export const Itinerary = (objArr) => {
         <section class="itinerary">
             <h4>Itinerary #${objArr.id}</h4>
             <ul class="itineraryItems">
-                <li>Destination: ${objArr.park}</li>
-                <li>Attraction: ${objArr.attraction}</li>
-                <li>Restaurant: ${objArr.eatery}</li>
+                <li><strong>Destination:</strong> ${objArr.park}</li>
+                <li><strong>Attraction:</strong> ${objArr.attraction}</li>
+                <li><strong>Restaurant:</strong> ${objArr.eatery}</li>
             </ul>
         </section>
     `
