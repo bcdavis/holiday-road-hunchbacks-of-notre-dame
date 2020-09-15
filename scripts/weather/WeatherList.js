ad6563ca67a9985413e330de6b56305c41e25eab
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-09-15 12:02:39 -0500

    updated Forecast

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -5,9 +5,9 @@ const eventHub = document.querySelector(".container");
 
 eventHub.addEventListener("parkChosen", (event) => {
   if (event.target.value !== "0") {
-    const lat = event.detail.lat
-    const long = event.detail.long
-    
+    const lat = event.detail.lat;
+    const long = event.detail.long;
+
     getWeather(lat, long).then((_) => {
       renderWeather(useWeather());
     });
@@ -17,13 +17,19 @@ eventHub.addEventListener("parkChosen", (event) => {
 const renderWeather = (weather) => {
   const contentTarget = document.querySelector(".parkWeather");
   contentTarget.innerHTML = `
-    <h3>5-Day Forecast</h3>
+  <h3>Today</h3>
+  <ul class="forecastList">
+  ${WeatherHTML(weather[0])}
+  </ul>
+  <h3>5-Day Forecast</h3>
+  `;
+  let i = 1;
+  while (i < 6) {
+    contentTarget.innerHTML += `
     <ul class="forecastList">
-    ${weather
-      .map((day) => {
-        return WeatherHTML(day);
-      })
-      .join("")}
+    ${WeatherHTML(weather[i])}
     </ul>
     `;
+    i++;
+  }
 };
