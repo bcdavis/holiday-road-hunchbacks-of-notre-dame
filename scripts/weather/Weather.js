a597aec14b652d61ff89673c8e38a4370a636402
Ricky Blake <rab0196@yahoo.com>
2020-09-15 09:10:12 -0500

    fixed spelling of precipitation

--- scripts/weather/Weather.js
+++ scripts/weather/Weather.js
@@ -8,7 +8,7 @@ export const WeatherHTML = (weather) => {
     const pop = Math.round(weather.pop * 100)
     return `
     <li>
-    ${new Date(date).toLocaleDateString("en-US")} -- High: ${Math.round(weather.temp.max)} -- Low: ${Math.round(weather.temp.min)} -- Chance of Prepcipitation: ${pop}%
+    ${new Date(date).toLocaleDateString("en-US")} -- High: ${Math.round(weather.temp.max)} -- Low: ${Math.round(weather.temp.min)} -- Chance of Precipitation: ${pop}%
     </li>
     `
 }
